1a88a0ae0545981e2ea7fa06fa7a90f80bc0ea44
0xGeN02 <manuelmateodgl02@gmail.com>
2023-03-28 17:46:32 +0200

    first-commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000000,
+    chunkSizeWarningLimit: 1000000
   }
 })
 

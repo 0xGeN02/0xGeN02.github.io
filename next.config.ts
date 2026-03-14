8fcbd38895bdbacca64590619b1bca8d9f0a92cf
0xGeN02 <manuelmateodgl02@gmail.com>
2026-03-14 12:37:22 +0100

    add google analytics

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const [repoOwner, repoName = "website"] =
-  process.env.GITHUB_REPOSITORY?.split("/") ?? [];
-const isUserOrOrgPagesRepo = repoName === `${repoOwner}.github.io`;
-const basePath = isGithubActions && !isUserOrOrgPagesRepo ? `/${repoName}` : "";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  basePath,
-  assetPrefix: basePath || undefined,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 

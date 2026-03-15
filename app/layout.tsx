19a6e7018d42c7c8cac0d35046d73279516ca389
0xGeN02 <manuelmateodgl02@gmail.com>
2026-03-15 04:11:01 +0100

    use static img

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import RouteHistoryTracker from "@/app/components/RouteHistoryTracker";
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { Metadata } from "next";
 import "./globals.css";
 
 const jetbrainsMono = JetBrains_Mono({
@@ -10,16 +10,32 @@ const jetbrainsMono = JetBrains_Mono({
   display: "swap",
 });
 
-
 export const metadata: Metadata = {
-  title: "0xGeN02 — terminal",
+  title: "0xGeN02",
   description: "Let's connect and build cool stuff together!",
   icons: { icon: "/favicon.ico" },
+  openGraph: {
+    title: "0xGeN02",
+    description: "Let's connect and build cool stuff together!",
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    siteName: "0xGeN02",
+    images: [
+      {
+        url: "/image/opengraph-image.png",
+        width: 1200,
+        height: 420,
+        alt: "0xGeN02 terminal banner",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
   twitter: {
     card: "summary_large_image",
-    title: "0xGeN02 — terminal",
+    title: "0xGeN02",
     description: "Let's connect and build cool stuff together!",
-  }
+    images: ["/image/opengraph-image.png"],
+  },
 };
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
